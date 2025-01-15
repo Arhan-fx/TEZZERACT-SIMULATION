f2ddf8a849ad825e1391dbab2d735c8f130a75cc
Mohd Arhan <m.arhanop@gmail.com>
2025-01-15 21:05:51 +0530

    Update tezzeract.py

--- tezzeract.py
+++ tezzeract.py
@@ -1,20 +1,20 @@
-def matrix_multiplication(a, b):
+def tezzeract_multiplication(a, b):
     columns_a = len(a[0])
     rows_a = len(a)
     columns_b = len(b[0])
     rows_b = len(b)
 
-    result_matrix = [[0 for j in range(columns_b)] for i in range(rows_a)]
+    result_tezzeract = [[j for j in range(columns_b)] for i in range(rows_a)]
     if columns_a == rows_b:
         for x in range(rows_a):
             for y in range(columns_b):
                 sum = 0
                 for k in range(columns_a):
                     sum += a[x][k] * b[k][y]
-                result_matrix[x][y] = sum
+                result_tezzeract[x][y] = sum
 
-        return result_matrix
+        return result_tezzeract
 
     else:
-        print("columns of the first matrix should be equal to the rows of the second matrix")
+        print("columns of the first matrix must be equal to the rows of the second matrix")
         return None
